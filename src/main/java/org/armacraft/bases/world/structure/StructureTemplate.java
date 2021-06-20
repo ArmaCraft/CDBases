2f5ed2102c653e973b06ec0e97b2b5a561e893bb
threader <40972803+localthreader@users.noreply.github.com>
2021-06-20 14:57:18 -0300

    Improving code

--- src/main/java/org/armacraft/bases/world/structure/StructureTemplate.java
+++ src/main/java/org/armacraft/bases/world/structure/StructureTemplate.java
@@ -5,7 +5,6 @@ import net.minecraft.util.math.vector.Vector3d;
 
 public class StructureTemplate {
     private int[][] template;
-    private Vector3d[][] relativeCoords;
 
     public StructureTemplate(int[]... template) {
         this.template = template;
@@ -16,15 +15,10 @@ public class StructureTemplate {
         for(int i = 0; i < template.length; i++) {
             for(int j = 0; j < template[i].length; j++) {
                 if(template[i][j] == 1) {
-                    if(relativeToX) {
-                        int relativePosX = pos.getX() + i;
-                        int relativePosY = pos.getY() + j;
-                        relativeCoords[i][j] = new Vector3d(relativePosX, relativePosY, pos.getZ());
-                    } else {
-                        int relativePosZ = pos.getZ() + i;
-                        int relativePosY = pos.getY() + j;
-                        relativeCoords[i][j] = new Vector3d(pos.getX(), relativePosY, relativePosZ);
-                    }
+                    int relativePosY = pos.getY() + j;
+                    relativeCoords[i][j] = relativeToX
+                            ? new Vector3d(pos.getX() + i, relativePosY, pos.getZ())
+                            : new Vector3d(pos.getX(), relativePosY, pos.getZ() + i);
                 }
             }
         }
