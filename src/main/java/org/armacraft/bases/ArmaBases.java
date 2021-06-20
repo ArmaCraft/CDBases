e1d75a892e01ca2d7247cde6ece30d920494a477
threader <40972803+localthreader@users.noreply.github.com>
2021-06-20 14:55:15 -0300

    Adding basic structure templates

--- src/main/java/org/armacraft/bases/ArmaBases.java
+++ src/main/java/org/armacraft/bases/ArmaBases.java
@@ -1,13 +1,18 @@
 package org.armacraft.bases;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.armacraft.bases.client.ClientDist;
 import org.armacraft.bases.server.ServerDist;
 import org.armacraft.bases.world.block.ModBlocks;
+import org.armacraft.bases.world.structure.ModStructureTemplates;
 import org.armacraft.bases.world.tileentity.ModTileEntities;
 
 @Mod(ArmaBases.MODID)
@@ -28,6 +33,21 @@ public class ArmaBases {
         ModBlocks.BLOCKS.register(modEventBus);
 
         this.dist = DistExecutor.safeRunForDist(() -> ClientDist::new, () -> ServerDist::new);
+    }
+
+    @SubscribeEvent
+    public void onRightClick(PlayerInteractEvent event) {
+        //Testing stuff
+        if(event.getPlayer().isCrouching()) {
+            Vector3d[][] structure = ModStructureTemplates.WALL_2X3.get().apply(event.getPos().above(), false);
+            for(int i = 0; i<structure.length; i++) {
+                for(int j = 0; j<structure[i].length; j++) {
+                    System.out.println(structure[i][j].toString());
+                }
+            }
+        } else {
+            Minecraft.getInstance().player.chat(event.getPos().toString());
+        }
     }
 
     public static ArmaBases instance() {
---
+++ src/main/java/org/armacraft/bases/world/structure/ModStructureTemplates.java
@@ -0,0 +1,28 @@
+package org.armacraft.bases.world.structure;
+
+import java.util.function.Supplier;
+
+public class ModStructureTemplates {
+    public static Supplier<StructureTemplate> SINGLE_BLOCK = () ->
+            new StructureTemplate(
+                    new int[]{1},
+                    new int[]{0});
+
+    public static Supplier<StructureTemplate> WALL_2X3 = () ->
+            new StructureTemplate(
+                    new int[]{1, 1},
+                    new int[]{1, 1},
+                    new int[]{1, 1});
+
+    public static Supplier<StructureTemplate> WALL_3X3 = () ->
+            new StructureTemplate(
+                    new int[]{1, 1, 1},
+                    new int[]{1, 1, 1},
+                    new int[]{1, 1, 1});
+
+    public static Supplier<StructureTemplate> DOOR = () ->
+            new StructureTemplate(
+                    new int[]{1, 1, 1},
+                    new int[]{0, 0, 1},
+                    new int[]{1, 1, 1});
+}
